0350b6a22b00e533ab0a3f4a19d23659138bdc76
Trevor Vaughan <tvaughan@onyxpoint.com>
2015-11-05 00:44:07 -0500

    (SIMP-557) Documentation Overhaul
    All of the pam::access::manage material was rewritten as well as much of
    the troubleshooting material.
    
    The SIMP Logo was added to the HTML docs.
    
    SIMP-557 #close #comment Updates for 4.2.X
    
    Change-Id: I48adbf0b473adb2c6f23842ded0d2bfede0bc334

--- docs/conf.py
+++ docs/conf.py
@@ -107,7 +107,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', '**/*.inc']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
@@ -170,7 +170,7 @@ html_title = "%s %s.%s documentation" % (project, version, release )
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = "images/SIMP_Logo.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
