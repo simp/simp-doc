77e62b75b571e48ad8d7979a41de48d9d97976ee
Chris Tessmer <chris.tessmer@onyxpoint.com>
2020-10-16 21:57:37 +0000

    Add syntax warning for :pupmod:`org/modulename`

--- docs/_extensions/simp_roles.py
+++ docs/_extensions/simp_roles.py
@@ -1,29 +1,31 @@
 import re
 from docutils import nodes
 from docutils.parsers.rst import roles
+import sphinx.errors
 
 def setup(app):
     app.add_role('github', auto_class( 'github', 'inline', 'https://github.com/%s'))
     app.add_role('jira', auto_class('jira', 'inline', 'https://simp-project.atlassian.net/browse/%s'))
     app.add_role('package', auto_class('package'))
-    app.add_role('pupmod', forge_class('pupmod'))
+    app.add_role('pupmod', pupmod_class())
     app.add_role('param', auto_class('param', 'literal'))
 
-
-def append_to_classes(options, role_name):
+# Ensure options dict contains a classes list with a 'simp-{role_name}' item
+def append_to_options_classes(options, role_name):
   roles.set_classes(options)
   if 'classes' not in options.keys():
     options['classes'] = []
   options['classes'].append( 'simp-%s' % role_name )
   return(options)
 
+# Add a :<role_name>:`<text>` role processor function
 def auto_class(role_name,role_type='inline',pattern=False):
   def role(name, rawtext, text, lineno, inliner, options={}, content=[]):
-    options = append_to_classes(options, role_name)
+    options = append_to_options_classes(options, role_name)
 
     if pattern:
       if role_type == 'literal':
-        options = append_to_classes(options, 'literal')
+        options = append_to_options_classes(options, 'literal')
       # From: https://protips.readthedocs.io/link-roles.html
       url = pattern % (text,)
       node = nodes.reference(rawtext, text, refuri=url, **options)
@@ -35,13 +37,19 @@ def auto_class(role_name,role_type='inline',pattern=False):
     return [node], []
   return role
 
-def forge_class(role_name, role_type='inline'):
+# Add a :pupmod:`<text>` role that auto-links "org-name" to the Puppet Forge
+def pupmod_class():
   def role(name, rawtext, text, lineno, inliner, options={}, content=[]):
-    options = append_to_classes(options, role_name)
+    options = append_to_options_classes(options, 'pupmod')
     forge_names = re.split("[-/]", text)
+
+    # Link to the module on Puppet Forge (if the text is in the right format)
     if len(forge_names) == 2:
       url = 'https://forge.puppet.com/%s/%s' % (forge_names[0], forge_names[1])
       node = nodes.reference(rawtext, text, refuri=url, **options)
+      if re.search('/', text):
+        fixed_text = re.sub('/', '-', text)
+        print("WARNING: Use '-' to separate Puppet module names (.e.g., %s', not '%s')\n" % (fixed_text, text))
     else:
       node = nodes.inline(rawtext, text, **options)
 
